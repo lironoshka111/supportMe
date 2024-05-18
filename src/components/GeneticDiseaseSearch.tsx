9be2906cec84fc7dae7d350d9489f013950b214f
liron <lironoshka111@gmail.com>
2024-05-18 23:31:51 +0300

    sf

--- src/components/GeneticDiseaseSearch.tsx
+++ src/components/GeneticDiseaseSearch.tsx
@@ -1,12 +1,63 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
+import { useCollection } from "react-firebase-hooks/firestore";
+import {addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase";
+import { useOwnDispatch } from "..";
+import { roomSelected } from "../redux/channelSlice";
 
 const GeneticDiseaseSearch: React.FC = () => {
+  const [snapshot, loading, error] = useCollection(collection(db, 'rooms'))
   const [options, setOptions] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
+  const dispatch = useOwnDispatch()
+  const [value, setValue] = React.useState<string | null>(options[0]);
+
+
+  const isRoomExist = (roomName: string) => {
+    let isExist = null
+    if(snapshot){
+      snapshot.docs.map(doc => {
+        if(doc.data().name === roomName){
+          console.log(doc,"lirob")
+          isExist = {name:doc.data().name, id: doc.id}
+        }
+      })
+    }
+    return isExist
+  }
+
+  const addChannel = async (name:string) => {
+    const res = await addDoc(collection(db, 'rooms'), {
+      name,
+    })
+    dispatch(
+        roomSelected({
+          id: res.id as string,
+          title:name as string,
+        }),
+    )
+  }
+
+  useEffect(() => {
+    if(!value){
+      return;
+    }
+    const room = isRoomExist(value);
+    if(room){
+        dispatch(
+            roomSelected({
+            id: room["id"] as string,
+            title: room["name"] as string,
+            }),
+        )
+    }else {
+        addChannel(value)
+    }
+  }, [value])
 
   const fetchDiseases = async (query: string) => {
     if (!query) return;
@@ -29,6 +80,10 @@ const GeneticDiseaseSearch: React.FC = () => {
       onInputChange={(event, newInputValue) => {
         setInputValue(newInputValue);
         fetchDiseases(newInputValue);
+      }}
+      value={value}
+      onChange={(event: any, newValue: string | null) => {
+        setValue(newValue);
       }}
       renderInput={(params) => (
         <TextField
