1833c1896fe6837a6894467358478bfefa38b29f
liron <lironoshka111@gmail.com>
2024-05-21 01:00:41 +0300

    style

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -8,6 +8,7 @@ import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import SendIcon from "@mui/icons-material/Send";
 import { useAuthState } from "react-firebase-hooks/auth";
+import EmojiPicker from "emoji-picker-react";
 
 interface ChatInputProps {
   roomId: string;
@@ -17,6 +18,7 @@ interface ChatInputProps {
 const ChatInput: React.FC<ChatInputProps> = ({ roomId, title }) => {
   const [user] = useAuthState(auth);
   const [messageValue, setMessageValue] = useState<string>("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
 
   const addMessage = async (e: FormEvent<HTMLFormElement>) => {
     if (messageValue) {
@@ -32,6 +34,10 @@ const ChatInput: React.FC<ChatInputProps> = ({ roomId, title }) => {
     }
   };
 
+  const onEmojiClick = (event: any) => {
+    setMessageValue((prevInput) => prevInput + event.emoji);
+  };
+
   return (
     <div className="flex w-[calc(100%-40px)] p-5">
       <Paper
@@ -39,9 +45,18 @@ const ChatInput: React.FC<ChatInputProps> = ({ roomId, title }) => {
         component="form"
         className="p-2.5 flex items-center w-full"
       >
-        <IconButton className="p-2.5" aria-label="menu">
+        <IconButton
+          className="p-2.5"
+          aria-label="menu"
+          onClick={() => setShowEmojiPicker((val) => !val)}
+        >
           <TagFacesIcon />
         </IconButton>
+        {showEmojiPicker && (
+          <div style={{ position: "absolute", bottom: "60px", left: "10px" }}>
+            <EmojiPicker onEmojiClick={onEmojiClick} />
+          </div>
+        )}
         <InputBase
           className="ml-2.5 flex-1"
           type="text"
